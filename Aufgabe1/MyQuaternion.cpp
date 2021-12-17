7d7e0e34704ab231e70e6b60f2e76c53aac59989
GGG_1_1 <39034992+TIGOA@users.noreply.github.com>
2021-12-17 17:27:26 +0100

    Add files via upload

---
+++ Aufgabe1/MyQuaternion.cpp
@@ -0,0 +1,24 @@
+#include"MyQuaternion.h"
+
+//Konstruktur
+MyQuaternion(double e1,double e2,double e3,double e4){};
+//Kopierkonstruktor
+MyQuaternion(vector<double> Q){}; 
+//addition operator +
+
+//subtraktion operator -
+
+//setter/getter operator []
+
+//get operator [] const
+
+//kopier operator=(Q_rhs)
+
+//zuweisung operator mit Skalar als argument operator=(skalar)
+
+//
+//conj() const
+
+//produkt zweier Quaternion, operator *
+
+//Skalierung einer Quaternion mit f  operator*(skalar)
---
+++ Aufgabe1/MyQuaternion.h
@@ -0,0 +1,41 @@
+
+#ifndef MyQuaternion_h
+#define MyQuaternion_h
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+class MyQuaternion
+{
+public:
+    
+    //Konstruktur
+    MyQuaternion(double e1,double e2,double e3,double e4);
+    //Kopierkonstruktor
+    MyQuaternion(vector<double> Q); 
+    //addition operator +
+    
+    //subtraktion operator -
+    
+    //setter/getter operator []
+    
+    //get operator [] const
+    
+    //kopier operator=(Q_rhs)
+    
+    //zuweisung operator mit Skalar als argument operator=(skalar)
+    
+    //
+    //conj() const
+    
+    //produkt zweier Quaternion, operator *
+    
+    //Skalierung einer Quaternion mit f  operator*(skalar)
+    
+    
+};
+
+#endif /* MyQuaternion_h */
+ 
