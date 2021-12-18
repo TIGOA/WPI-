756f0b6d5dae309ab97085fc74d82b1575d43cad
GGG_1_1 <39034992+TIGOA@users.noreply.github.com>
2021-12-18 01:09:11 +0100

    Update MyQuaternion.h

--- Aufgabe1/MyQuaternion.h
+++ Aufgabe1/MyQuaternion.h
@@ -11,14 +11,15 @@ class MyQuaternion
 {
 public:
     
+    double e1, e2, e3, e4;
     //Konstruktur
     MyQuaternion(double e1,double e2,double e3,double e4);
     //Kopierkonstruktor
-    MyQuaternion(vector<double> Q); 
+    MyQuaternion(const MyQuaternion &Q); 
     //addition operator +
-    
+    void operator+(const MyQuaternion &Q);
     //subtraktion operator -
-    
+    void operator-(const MyQuaternion &Q);
     //setter/getter operator []
     
     //get operator [] const
@@ -29,7 +30,7 @@ public:
     
     //
     //conj() const
-    
+    MyQuaternion conj() const;
     //produkt zweier Quaternion, operator *
     
     //Skalierung einer Quaternion mit f  operator*(skalar)
