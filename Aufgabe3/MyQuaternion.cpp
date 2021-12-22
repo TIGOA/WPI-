7663056bbb3c1bf5bf28401867b2e8f94e7404cf
GGG_1_1 <39034992+TIGOA@users.noreply.github.com>
2021-12-22 13:04:02 +0100

    Add files via upload

---
+++ Aufgabe3/MyQuaternion.cpp
@@ -0,0 +1,206 @@
+#include"MyQuaternion.h"
+
+//default konstruktur
+MyQuaternion::MyQuaternion() {
+    this->Quaternion[0] = 0.;
+    this->Quaternion[1] = 0.;
+    this->Quaternion[2] = 0.;
+    this->Quaternion[3] = 0.;
+}
+
+//Konstruktur
+MyQuaternion::MyQuaternion(const double e1,const double e2,const double e3,const double e4)
+{
+        this->Quaternion[0] = e1;
+        this->Quaternion[1] = e2;
+        this->Quaternion[2] = e3;
+        this->Quaternion[3] = e4;
+};
+//Kopierkonstruktor
+MyQuaternion::MyQuaternion(const MyQuaternion &Q)
+{
+        this->Quaternion[0]=Q.Quaternion[0];
+        this->Quaternion[1]=Q.Quaternion[1];
+        this->Quaternion[2]=Q.Quaternion[2];
+        this->Quaternion[3]=Q.Quaternion[3];
+};
+
+
+//addition operator +
+MyQuaternion MyQuaternion::operator+(const MyQuaternion &Q)const
+{
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<4; i++)
+    {
+        result.Quaternion[i]+=Q.Quaternion[i];
+    }
+    
+    return result;
+    
+}
+//subtraktion operator -
+MyQuaternion MyQuaternion::operator-(const MyQuaternion &Q)const
+{
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<4; i++)
+    {
+        result.Quaternion[i]-=Q.Quaternion[i];
+    }
+    
+    return result;
+}
+
+//setter/getter operator []
+double & MyQuaternion::operator[] (const int index)
+{
+    return this->Quaternion[index];
+}
+
+//get operator [] const
+const double MyQuaternion::operator[] (const int index) const
+{
+    return this->Quaternion[index];
+}
+
+
+
+// copy operator
+MyQuaternion& MyQuaternion::operator=(const MyQuaternion & Q) {
+   
+    for (auto i=0; i<4; i++)
+        {
+            this->Quaternion[i]=Q.Quaternion[i];
+        }
+    return *this;
+}
+
+//produkt zweier Quaternion, operator *
+
+const MyQuaternion MyQuaternion::operator* (const MyQuaternion & Q) const{
+    
+    MyQuaternion result =*this;
+    
+    
+    
+    const double e1 = result.Quaternion[0]*Q.Quaternion[3]
+                    + result.Quaternion[3]*Q.Quaternion[0]
+                    - result.Quaternion[1]*Q.Quaternion[2]
+                    + result.Quaternion[2]*Q.Quaternion[1];
+    const double e2 = result.Quaternion[1]*Q.Quaternion[3]
+                    + result.Quaternion[3]*Q.Quaternion[1]
+                    - result.Quaternion[2]*Q.Quaternion[0]
+                    + result.Quaternion[0]*Q.Quaternion[2];
+    const double e3 = result.Quaternion[2]*Q.Quaternion[3]
+                    + result.Quaternion[3]*Q.Quaternion[2]
+                    - result.Quaternion[0]*Q.Quaternion[1]
+                    + result.Quaternion[1]*Q.Quaternion[0];
+    const double e4 = result.Quaternion[3]*Q.Quaternion[3]
+                    - result.Quaternion[0]*Q.Quaternion[0]
+                    - result.Quaternion[1]*Q.Quaternion[1]
+                    - result.Quaternion[2]*Q.Quaternion[2];
+    
+    std::vector<double> e = {e1,e2,e3,e4};
+    
+   
+    
+    for(auto i=0;i<4;i++)
+    {
+        result.Quaternion[i]=e[i];
+    }
+    
+    return result;
+}
+
+
+
+
+
+
+//Skalierung operator* (skalar)
+MyQuaternion MyQuaternion::operator* (const double & fliesszahl) const{
+    
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<4; i++)
+    {
+        result.Quaternion[i]=fliesszahl*this->Quaternion[i];
+    }
+    
+    return result;
+}
+
+
+// operator = zuweisung
+MyQuaternion & MyQuaternion::operator= (const double value){
+   
+    for (auto i=0; i<4; i++)
+        {
+            this->Quaternion[i]=value;
+        }
+    return *this;
+    
+    
+}
+
+
+//Konjugiert
+const MyQuaternion MyQuaternion::conj(const int w) const{
+    
+    
+    
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<w-1; i++)
+    {
+        result.Quaternion[i]=-1*this->Quaternion[i];
+        result.Quaternion[w-1]=this->Quaternion[w-1];
+        
+    }
+    
+    return result;
+    
+    
+}
+
+// divide by scalar
+const MyQuaternion MyQuaternion::operator/ (double n) const{
+   
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<4; i++)
+    {
+        result.Quaternion[i]=this->Quaternion[i]/n;
+    }
+    
+    return result;
+    
+}
+
+// norm of Quaternion
+const MyQuaternion MyQuaternion::norm() const{
+    double Q_betrag_sqrt;
+    Q_betrag_sqrt=sqrt(this->Quaternion[0]*this->Quaternion[0]
+               +this->Quaternion[1]*this->Quaternion[1]
+               +this->Quaternion[2]*this->Quaternion[2]
+               +this->Quaternion[3]*this->Quaternion[3]);
+   
+    MyQuaternion result =*this;
+    
+    for (auto i=0; i<4; i++)
+    {
+        result.Quaternion[i]=this->Quaternion[i]/Q_betrag_sqrt;
+    }
+    
+    return result;
+}
+
+// norm of Quaternion
+double MyQuaternion::betrag() const{
+    
+    return sqrt(this->Quaternion[0]*this->Quaternion[0]
+               +this->Quaternion[1]*this->Quaternion[1]
+               +this->Quaternion[2]*this->Quaternion[2]
+               +this->Quaternion[3]*this->Quaternion[3]);
+}
---
+++ Aufgabe3/MyQuaternion.h
@@ -0,0 +1,63 @@
+
+#ifndef MyQuaternion_h
+#define MyQuaternion_h
+#include <iostream>
+#include <vector>
+#include <cmath> // for norm of Quaternion
+
+
+using namespace std;
+
+
+
+class MyQuaternion
+{
+public:
+    
+   // double e1, e2, e3, e4;
+    double Quaternion[4];
+    
+    //default setting
+    MyQuaternion();
+    //Konstruktur
+    MyQuaternion(const double e1,const double e2,const double e3,const double e4);
+    //Kopierkonstruktor
+    MyQuaternion(const MyQuaternion &Q);
+    
+    
+    //setter/getter operator []
+    double & operator[] (const int index);
+    //get operator [] const ,read only
+    const double operator[](const int index)const;
+   
+    
+    
+    //addition operator +
+    MyQuaternion operator+(const MyQuaternion &Q)const;
+    //subtraktion operator -
+    MyQuaternion operator-(const MyQuaternion &Q)const;
+    //Skalierung einer Quaternion mit f  operator*(skalar)
+    MyQuaternion operator* (const double & fliesszahl) const;
+    
+    //kopier operator=(Q_rhs)
+    MyQuaternion & operator= (const MyQuaternion & Q);
+
+    //produkt operator *
+    const MyQuaternion operator* (const MyQuaternion & Qb) const;
+    
+    // argument with scalar initial
+    MyQuaternion & operator= (const double value);
+    // conjugate Quaternion
+    const MyQuaternion conj(const int w = 4) const;
+    // divide by scalar
+    const MyQuaternion operator/ (double n) const;
+    // norm of Quaternion
+    const MyQuaternion norm() const;
+    
+    // betrag of Quaternion
+    double betrag() const;
+    
+};
+
+#endif /* MyQuaternion_h */
+ 
