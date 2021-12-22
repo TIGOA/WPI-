cd3cb78f32d304779a8d3fe930b77377d972457a
GGG_1_1 <39034992+TIGOA@users.noreply.github.com>
2021-12-22 13:02:44 +0100

    Update MyFunctions.cpp

--- Aufgabe3/MyFunctions.cpp
+++ Aufgabe3/MyFunctions.cpp
@@ -8,17 +8,50 @@
 #include <cmath>
 #include <iostream>
 #include "MyFunctions.h"
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
 MyQuaternion quaternion_rotation(double x,double y,double z,double const theta){
-	// aus Aufgabe 2
+
+  // Quellcode implememtieren
+    
+       // Achse normiereren
+        double xn = x/sqrt(x*x+y*y+z*z);
+        double yn = y/sqrt(x*x+y*y+z*z);
+        double zn = z/sqrt(x*x+y*y+z*z);
+    
+        // berechnen Einheitsquaternion
+        const double e1 = xn*sin(theta*PI/360.);
+        const double e2 = yn*sin(theta*PI/360.);
+        const double e3 = zn*sin(theta*PI/360.);
+        const double e4 = 1*cos(theta*PI/360.);
+    
+        MyQuaternion result(e1,e2,e3,e4);
+        return result;
+    
+  
 }
 
 MyQuaternion rotateX(MyQuaternion const &X,MyQuaternion const &Q){
-// aus Aufgabe 2 übernehmen
+  // Quellcode implementieren: und auch in Aufgabe3 einfügen
+    MyQuaternion Q_Inverse;
+    MyQuaternion X_rot;
+        // inverse of Q
+    Q_Inverse = Q.conj()/Q.betrag();
+//    Q_Inverse = Q.conj()/Q.norm();
+    
+        
+        X_rot = Q*X*Q_Inverse;
+        return X_rot;
 }
 
+
+/*
+ *  vorgegeben:
+ *
+ */
 void Qprint(const MyQuaternion &Q,const std::string txt=""){
 	cout<<txt<<"("<<Q[0]<<","<<Q[1]<<","<<Q[2]<<","<<Q[3]<<")"<<endl;
 }
